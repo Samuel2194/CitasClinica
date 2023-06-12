13fea65bee369953ceee358812675500c6dcd0c0
Sam <samuel.knd.9@gmail.com>
2023-06-11 19:44:34 -0600

    Verifica configuracion requerida para poder ejecutarse el programa

---
+++ src/CitasClinicas/Admin.java
@@ -0,0 +1,64 @@
+package CitasClinicas;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Admin {
+    public  BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+    File fileAdmin = new File(".\\src\\CitasClinicas\\db\\Admin.csv");
+    HashMap<String, String> administradores;
+    public Admin(){
+        administradores = new HashMap<String, String>();
+    }
+
+    public boolean Load(){
+        boolean result =false;
+        File path = new File(".\\src\\CitasClinicas\\db");
+        String[] administrador;
+        if (!path.exists()) {
+            if (path.mkdirs()) {
+                System.out.println("Directorio creado");
+            } else {
+                System.out.println("Error al crear directorio");
+            }
+        }
+        try {
+            fileAdmin.createNewFile();
+        } catch (IOException e) {
+        }
+        try {
+            Scanner sc = new Scanner(fileAdmin);
+            sc.useDelimiter(",");
+            while (sc.hasNextLine())
+            {
+                administrador= sc.nextLine().split(",");
+                NewAdmin(administrador[0],administrador[1]);
+            }
+            sc.close();
+            result = true;
+        }catch (Exception e) {
+            result = false;
+        }
+        return result;
+    }
+
+    public String NewAdmin(String usuario, String pass) {
+        String result="";
+        try {
+            administradores.put(usuario,pass);
+            result= "El administrador se ha agregado";
+        }catch (Exception e){
+            result ="Ocurrio un problema al crear el administrador";
+        }
+
+        return result;
+    }
+
+    public int CountAdmin(){
+        return administradores.size();
+    }
+}
--- src/CitasClinicas/Main.java
+++ src/CitasClinicas/Main.java
@@ -1,11 +1,18 @@
+package CitasClinicas;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     public static BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+    static Admin admin= new Admin();
     static String opcion="";
     public static void main(String[] args) throws IOException {
+        admin.Load();
+        if (admin.CountAdmin()==0){
+            StartConfig();
+        }
         System.out.println("¡Bienvenido!\n¿Deseas ingresar como administrador?\n 1) Si\n 2) No\n 3) Salir");
         opcion = scanner.readLine();
         switch(opcion.toUpperCase()) {
@@ -31,7 +38,7 @@ public class Main {
 
     public static void MenuAdmin() throws IOException {
 
-        System.out.println("¿Que deseas hacer?\n 1) Alta Doctores\n 2) Alta Pacientes\n 3) Eliminar Contacto\n 4) Salir");
+        System.out.println("¿Que deseas hacer?\n 1) Alta Usuarios\n 2) Asignar Doctor\n 3) Salir");
         opcion = scanner.readLine();
         switch(opcion.toUpperCase()) {
             case "1": {
@@ -62,5 +69,19 @@ public class Main {
 
     public static void MenuUsuario(){
 
+    }
+
+    public static void StartConfig() throws IOException {
+        String nombre, pass, repPass;
+        System.out.println("Es necesario estableces un administrador para que se pueda ejecutar el programa");
+        System.out.println("Ingresa el nombre del Administrador");
+        nombre = scanner.readLine();
+        System.out.println("Ingresa la contrasela");
+        pass = scanner.readLine();
+        System.out.println("Repite la contrasela");
+        repPass = scanner.readLine();
+        if (pass==repPass){
+            System.out.println (admin.NewAdmin(nombre, pass));
+        }
     }
 }
